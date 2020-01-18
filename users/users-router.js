b7beb56501c47ba81f9e918029cc0d87efa89a39
Adam <adamcpenman@gmail.com>
2020-01-18 12:00:42 -0600

    added boiler plate for index.js

--- users/users-router.js
+++ users/users-router.js
@@ -4,33 +4,6 @@ const bcrypt = require("bcryptjs")
 
 const router = express.Router()
 
-// function restricted() {
-//     const authError = {message: "You can't be here"}
-//     return async ( req, res, next) => {
-//         try {
-//             const {username, password} = req.headers
-//                 if (!username || !password) {
-//                     return res.status(401).json(authError)
-                  
-//                 }
-//         const user = await usersModel.findBy({ username }).first()
-//             if (!user) {
-//                 return res.status(401).json(authError)
-//             }
-//         const passwordValid = await bcrypt.compare(password, user.password)
-//             if (!passwordValid) {
-//                 return res.status(401).json(authError)
-//             }
-
-//             const users = await usersModel.find()
-
-//             res.json(users)
-//         } catch(err) {
-//             next(err)
-//         }
-//     }
-// }
-
 router.get("/users", async  (req, res, next) => {
 const authError = {message: "You can't be here"}
    
